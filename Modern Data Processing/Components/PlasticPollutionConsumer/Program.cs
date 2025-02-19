fbd7dc32d1fa6a3a6de4010b9b127adbf21cbd56
Vladyslav Bardin <vbardin810@gmail.com>
2025-02-19 15:32:07 +0200

    feat: Add kafka topic consumer for plastic pollution topic

---
+++ Modern Data Processing/Components/PlasticPollutionConsumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+
+const string topicName = "int.streaming.plastic.pollution";
+KafkaConsumer.Consume(topicName);
+
+internal static class KafkaConsumer
+{
+    private static readonly ConsumerConfig ConsumerConfig = new()
+    {
+        BootstrapServers = "PLAINTEXT://localhost:19092,PLAINTEXT://localhost:29092,PLAINTEXT://localhost:39092",
+        Acks = Acks.None,
+        GroupId = "PlasticPollution-RealTime-EventsLog",
+        AutoOffsetReset = AutoOffsetReset.Earliest,
+        EnableAutoCommit = true
+    };
+
+    private static readonly IConsumer<string, string> Consumer =
+        new ConsumerBuilder<string, string>(ConsumerConfig).Build();
+
+    public static void Consume(string topic)
+    {
+        Consumer.Subscribe(topic);
+
+        Console.WriteLine($"🎯 Listening for messages on topic: {topic}...");
+
+        try
+        {
+            while (true)
+            {
+                try
+                {
+                    var consumeResult = Consumer.Consume(CancellationToken.None);
+                    var jsonMessage = consumeResult.Message.Value;
+
+                    Console.WriteLine($"✅ Received Message: {jsonMessage}");
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"❌ Kafka Consume Error: {e.Error.Reason}");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("🛑 Consumer shutting down...");
+        }
+        finally
+        {
+            Consumer.Close();
+        }
+    }
+}
