1ddda85b73ee03626924b6258df68a8d4b5831f2
Vladyslav Bardin <vbardin810@gmail.com>
2025-02-20 03:01:39 +0200

    feat: Improve consumer & add PG CDC topic consumer

--- Modern Data Processing/Components/PlasticPollutionConsumer/Program.cs
+++ Modern Data Processing/Components/PlasticPollutionConsumer/Program.cs
@@ -1,27 +1,40 @@
 ﻿using Confluent.Kafka;
 
-const string topicName = "int.streaming.plastic.pollution";
-KafkaConsumer.Consume(topicName);
+const string kafkaProducerTopic = "int.streaming.plastic.pollution";
+const string pgCdcTopic = "int.streaming.pg.public.plastics";
 
-internal static class KafkaConsumer
+var task1 = Task.Run(() => new KafkaConsumer(kafkaProducerTopic).Consume());
+var task2 = Task.Run(() => new KafkaConsumer(pgCdcTopic).Consume());
+
+await Task.WhenAll(task1, task2);
+
+internal sealed class KafkaConsumer
 {
-    private static readonly ConsumerConfig ConsumerConfig = new()
+    private readonly string _topic;
+    private readonly IConsumer<string, string> _consumer;
+
+    public KafkaConsumer(string topic)
     {
-        BootstrapServers = "PLAINTEXT://localhost:19092,PLAINTEXT://localhost:29092,PLAINTEXT://localhost:39092",
-        Acks = Acks.None,
-        GroupId = "PlasticPollution-RealTime-EventsLog",
-        AutoOffsetReset = AutoOffsetReset.Earliest,
-        EnableAutoCommit = true
-    };
+        _topic = topic;
 
-    private static readonly IConsumer<string, string> Consumer =
-        new ConsumerBuilder<string, string>(ConsumerConfig).Build();
+        var consumerConfig= new ConsumerConfig
+        {
+            BootstrapServers = "PLAINTEXT://localhost:19092,PLAINTEXT://localhost:29092,PLAINTEXT://localhost:39092",
+            Acks = Acks.Leader,
+            GroupId = "PlasticPollution-RealTime-EventsLog-" + topic,
+            AutoOffsetReset = AutoOffsetReset.Latest,
+            EnableAutoCommit = true,
+            AutoCommitIntervalMs = 1000,
+        };
 
-    public static void Consume(string topic)
+        _consumer = new ConsumerBuilder<string, string>(consumerConfig).Build();
+    }
+
+    public void Consume()
     {
-        Consumer.Subscribe(topic);
+        _consumer.Subscribe(_topic);
 
-        Console.WriteLine($"🎯 Listening for messages on topic: {topic}...");
+        Console.WriteLine($"🎯 Listening for messages on topic: {_topic}...");
 
         try
         {
@@ -29,7 +42,7 @@ internal static class KafkaConsumer
             {
                 try
                 {
-                    var consumeResult = Consumer.Consume(CancellationToken.None);
+                    var consumeResult = _consumer.Consume(CancellationToken.None);
                     var jsonMessage = consumeResult.Message.Value;
 
                     Console.WriteLine($"✅ Received Message: {jsonMessage}");
@@ -46,7 +59,7 @@ internal static class KafkaConsumer
         }
         finally
         {
-            Consumer.Close();
+            _consumer.Close();
         }
     }
 }
