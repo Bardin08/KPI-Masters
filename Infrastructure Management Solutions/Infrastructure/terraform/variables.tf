ef16e41c023e67688a17b47f655f6709aada5f81
Vladyslav Bardin <vbardin810@gmail.com>
2025-02-24 16:49:58 +0200

    feat: Add provisioners to configure newly created VM

--- Infrastructure Management Solutions/Infrastructure/terraform/variables.tf
+++ Infrastructure Management Solutions/Infrastructure/terraform/variables.tf
@@ -7,3 +7,13 @@ variable "ssh_public_key" {
   description = "SSH public key for accessing the droplet"
   type        = string
 }
+
+variable "ssh_private_key_path" {
+  description = "SSH private key for accessing the droplet"
+  type        = string
+}
+
+variable "ssh_key_fingerprint" {
+  description = "SSH key fingerprint"
+  type        = string
+}
